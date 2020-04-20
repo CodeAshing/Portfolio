bc1b06389114f31a98ced35195c6b0ebc9efb5a8
asharib90 <asharibahmed143@gmial.com>
2020-04-20 00:28:34 -0700

    Updates

---
+++ precache-manifest.8097ea72f610db2e907d0e036a674683.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df94b9578aa67037925480b96faa44b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "064e2fe8af4a9ad33cdd",
+    "url": "/Portfolio/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "16b74e2da0b5364906b7",
+    "url": "/Portfolio/static/css/main.2c905d29.chunk.css"
+  },
+  {
+    "revision": "064e2fe8af4a9ad33cdd",
+    "url": "/Portfolio/static/js/2.a22bed16.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Portfolio/static/js/2.a22bed16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16b74e2da0b5364906b7",
+    "url": "/Portfolio/static/js/main.4549debb.chunk.js"
+  },
+  {
+    "revision": "53b75a8283d445246ce3",
+    "url": "/Portfolio/static/js/runtime-main.52909098.js"
+  }
+]);
